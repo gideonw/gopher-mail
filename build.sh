532017f725ae5aa57fc199ee9f1c1b278ff59d8b
Gideon Williams <gideon@gideonw.com>
2020-05-19 03:39:32 -0700

    Update build scripts

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
-#! /bin/bash
+#! /usr/bin/env /bin/bash
 
 echo "[INFO] Building postmaster..."
 
@@ -14,3 +14,9 @@ cd ../mailtruck
 env GOOS=linux go build -ldflags="-s -w" -o ../bin/mailtruck main.go
 cd ../
 
+echo "[INFO] Archiving postmaster..."
+zip ./bin/postmaster.zip ./bin/postmaster
+echo "[INFO] Archiving mailman..."
+zip ./bin/mailman.zip ./bin/mailman 
+echo "[INFO] Archiving mailtruck..."
+zip ./bin/mailtruck.zip ./bin/mailtruck 
--- deploy.sh
+++ deploy.sh
@@ -14,5 +14,4 @@ cd ../
 
 echo "[INFO] Uploading postmaster to $LAMBDA_ARCHIVE_BUCKET/postmaster/"
 
-gzip -fk ./bin/postmaster > ./bin/postmaster.zip
 aws s3 cp ./bin/postmaster.zip s3://$LAMBDA_ARCHIVE_BUCKET/postmaster/postmaster.zip
