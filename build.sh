60ec7c0cc1a8bb7fed4f7ea5da0e3cf8efc83755
Gideon Williams <gideon@gideonw.com>
2020-05-19 05:10:23 -0700

    Update build scripts

--- build.sh
+++ build.sh
@@ -3,20 +3,35 @@
 echo "[INFO] Building postmaster..."
 
 cd postmaster
-env GOOS=linux go build -ldflags="-s -w" -o ../bin/postmaster main.go sns.go
+env GOOS=linux GOARCH=amd64 go build -o ../bin/postmaster main.go sns.go
 
 echo "[INFO] Building mailman..."
 cd ../mailman
-env GOOS=linux go build -ldflags="-s -w" -o ../bin/mailman main.go
+env GOOS=linux GOARCH=amd64 go build -o ../bin/mailman main.go
 
 echo "[INFO] Building mailtruck..."
 cd ../mailtruck
-env GOOS=linux go build -ldflags="-s -w" -o ../bin/mailtruck main.go
+env GOOS=linux GOARCH=amd64 go build -o ../bin/mailtruck main.go
 cd ../
 
+# TODO: On windows require build-lambda-zip be installed.
+
+cd ./bin
 echo "[INFO] Archiving postmaster..."
-zip ./bin/postmaster.zip ./bin/postmaster
+rm postmaster.zip
+chmod +x postmaster 
+# zip -j postmaster.zip postmaster 
+build-lambda-zip --output postmaster.zip postmaster 
+
 echo "[INFO] Archiving mailman..."
-zip ./bin/mailman.zip ./bin/mailman 
+rm mailman.zip
+chmod +x mailman 
+# zip -j mailman.zip mailman 
+build-lambda-zip --output mailman.zip mailman 
+
 echo "[INFO] Archiving mailtruck..."
-zip ./bin/mailtruck.zip ./bin/mailtruck 
+rm mailtruck.zip
+chmod +x mailtruck
+# zip -j mailtruck.zip mailtruck 
+build-lambda-zip --output mailtruck.zip mailtruck 
+cd ../
--- deploy.sh
+++ deploy.sh
@@ -13,5 +13,10 @@ cd ../
 # Deploy
 
 echo "[INFO] Uploading postmaster to $LAMBDA_ARCHIVE_BUCKET/postmaster/"
-
 aws s3 cp ./bin/postmaster.zip s3://$LAMBDA_ARCHIVE_BUCKET/postmaster/postmaster.zip
+
+echo "[INFO] Uploading mailman to $LAMBDA_ARCHIVE_BUCKET/mailman/"
+aws s3 cp ./bin/mailman.zip s3://$LAMBDA_ARCHIVE_BUCKET/mailman/mailman.zip
+
+echo "[INFO] Uploading mailtruck to $LAMBDA_ARCHIVE_BUCKET/mailtruck/"
+aws s3 cp ./bin/mailtruck.zip s3://$LAMBDA_ARCHIVE_BUCKET/mailtruck/mailtruck.zip
